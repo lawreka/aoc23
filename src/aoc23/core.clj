1370d35b9f355cfac5bf40ce293e45080025fdc1
Kathryn Isabelle Lawrence <lawr3ka@gmail.com>
2023-12-01 18:32:55 +0100

    day 1 done

---
+++ src/aoc23/core.clj
@@ -0,0 +1,9 @@
+(ns aoc23.core
+  (:gen-class))
+
+(defn main
+  "Jacking in (⌐■_■)"
+  []
+  (println "*hacker voice* im in"))
+
+(main)
---
+++ test/aoc23/core_test.clj
@@ -0,0 +1,7 @@
+(ns aoc23.core-test
+  (:require [clojure.test :refer :all]
+            [aoc23.core :refer :all]))
+
+(deftest a-test
+  (testing "Up and runnin"
+    (is (= true true))))
