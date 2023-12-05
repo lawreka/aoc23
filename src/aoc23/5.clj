36ab329983d734f68f22a49eac841ab73e9786f2
Kathryn Isabelle Lawrence <lawr3ka@gmail.com>
2023-12-05 22:18:16 +0100

    day 5 part 2

--- src/aoc23/5.clj
+++ src/aoc23/5.clj
@@ -115,3 +115,91 @@
 ;; expect => 35
 (solve input)
 ;; returns => 340994526
+
+
+(defn input->ranges2 [input]
+  (let [seeds (-> input
+                  str/split-lines
+                  first
+                  (str/split #":")
+                  last
+                  str/trim
+                  (str/split #" "))
+        more-seeds (->> (map (fn [seed-range]
+                               (let [start (Long/parseLong (first seed-range))
+                                     length (Long/parseLong (last seed-range))]
+                                 {:start start
+                                  :end (dec (+ start length))}))
+                             (partition 2 seeds))
+                        flatten)
+        seed-to-soil (-> (map->range input 1)
+                         (trim-ranges-only true)
+                         populate-helpful-data-for-ranges)
+        soil-to-fertilizer (-> (map->range input 2)
+                               (trim-ranges-only true)
+                               populate-helpful-data-for-ranges)
+        fertilizer-to-water (-> (map->range input 3)
+                                (trim-ranges-only true)
+                                populate-helpful-data-for-ranges)
+        water-to-light (-> (map->range input 4)
+                           (trim-ranges-only true)
+                           populate-helpful-data-for-ranges)
+        light-to-temperature (-> (map->range input 5)
+                                 (trim-ranges-only true)
+                                 populate-helpful-data-for-ranges)
+        temperature-to-humidity (-> (map->range input 6)
+                                    (trim-ranges-only true)
+                                    populate-helpful-data-for-ranges)
+        humidity-to-location (-> (map->range input 7)
+                                 (trim-ranges-only true)
+                                 populate-helpful-data-for-ranges)]
+    {:seeds more-seeds
+     :seed-to-soil {:ranges seed-to-soil}
+     :soil-to-fertilizer {:ranges soil-to-fertilizer}
+     :fertilizer-to-water {:ranges fertilizer-to-water}
+     :water-to-light {:ranges water-to-light}
+     :light-to-temperature {:ranges light-to-temperature}
+     :temperature-to-humidity {:ranges temperature-to-humidity}
+     :humidity-to-location {:ranges humidity-to-location}}))
+
+(defn get-location-number
+  [seed-number map-of-ranges]
+  (let [get-in-next-range (fn [seed next-range]
+                            (let [next-num (seed->soil seed next-range)]
+                              next-num))
+        soil-number (get-in-next-range seed-number (get-in map-of-ranges [:seed-to-soil :ranges]))
+        fertilizer-number (get-in-next-range soil-number (get-in map-of-ranges [:soil-to-fertilizer :ranges]))
+        water-number (get-in-next-range fertilizer-number (get-in map-of-ranges [:fertilizer-to-water :ranges]))
+        light-number (get-in-next-range water-number (get-in map-of-ranges [:water-to-light :ranges]))
+        temperature-number (get-in-next-range light-number (get-in map-of-ranges [:light-to-temperature :ranges]))
+        humidity-number (get-in-next-range temperature-number (get-in map-of-ranges [:temperature-to-humidity :ranges]))
+        location-number (get-in-next-range humidity-number (get-in map-of-ranges [:humidity-to-location :ranges]))]
+    location-number))
+
+(defn process-seed-range
+  ([seed-range map-of-ranges]
+   (let [start (:start seed-range)
+         end (:end seed-range)
+         first-location-number (get-location-number start map-of-ranges)]
+     (process-seed-range map-of-ranges start end first-location-number)))
+  ([map-of-ranges current-seed end result]
+   (let [location-number (get-location-number current-seed map-of-ranges)
+         new-result (min result location-number)]
+     (println :seed current-seed :location location-number)
+     (if (not (> current-seed end))
+       (recur map-of-ranges (inc current-seed) end new-result)
+       new-result))))
+
+(defn brute-force
+  "DOES NOT WORK FOR REAL INPUT"
+  [input]
+  (let [map-of-ranges (input->ranges2 input)
+        min-in-ranges (map (fn [seed-range]
+                             (process-seed-range seed-range map-of-ranges))
+                           (:seeds map-of-ranges))]
+    (apply min min-in-ranges)))
+
+(brute-force test-input)
+;; expect => 46
+
+#_(solve2 input)
