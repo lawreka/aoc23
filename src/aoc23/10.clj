d73730e583fd2fae3d771b7569faf4d6a2886353
Kathryn Isabelle Lawrence <kathryn@pitch.io>
2023-12-16 16:00:31 +0100

    work on day 10

--- src/aoc23/10.clj
+++ src/aoc23/10.clj
@@ -14,6 +14,26 @@
 ;; . is ground; there is no pipe in this tile.
 ;; S is the starting position of the animal; there is a pipe on this tile, but your sketch doesn't show what shape the pipe has.
 
+(def north-connectors #{"S" "|" "7" "F"})
+(def south-connectors #{"S" "|" "L" "J"})
+(def east-connectors #{"S" "-" "J" "7"})
+(def west-connectors #{"S" "-" "L" "F"})
+
+(defn connected? [current-symbol next-symbol direction]
+  (cond
+    (= direction :north) (and (contains? south-connectors current-symbol)
+                              (contains? north-connectors next-symbol))
+    (= direction :south) (and (contains? north-connectors current-symbol)
+                              (contains? south-connectors next-symbol))
+    (= direction :east) (and (contains? west-connectors current-symbol)
+                             (contains? east-connectors next-symbol))
+    (= direction :west) (and (contains? east-connectors current-symbol)
+                                   (contains? west-connectors next-symbol))
+    :else false))
+
+(connected? "S" "|" :north)
+(connected? "|" "-" :east)
+
 (defn solve
   [input]
   (let [lines (map-indexed (fn [index line]
@@ -21,21 +41,34 @@
                               :line line})
                            (str/split-lines input))
         s-line (-> (filter #(str/includes? (:line %) "S") lines)
-                       first) 
+                   first) 
         s-index (str/index-of (:line s-line) "S")
         get-path (fn [line next-line prev-line pos-index]
-                   (let [north-node (nth prev-line pos-index)
-                         south-node (nth next-line pos-index)
-                         east-node (nth line (inc pos-index))
-                         west-node (nth line (dec pos-index))]
-                     {:north north-node :south south-node :east east-node :west west-node}))
+                   (let [line-length (count line)
+                         _ (print line-length)
+                         current-symbol (-> (nth line pos-index)
+                                            str)
+                         north-node (-> (nth prev-line pos-index)
+                                        str)
+                         north? (connected? current-symbol north-node :north)
+                         south-node (-> (nth next-line pos-index)
+                                        str)
+                         south? (connected? current-symbol south-node :south)
+                         east-node (-> (nth line (inc pos-index))
+                                       str)
+                         east? (connected? current-symbol east-node :east)
+                         west-node (-> (nth line (max (dec pos-index) 0))
+                                       str)
+                         west? (connected? current-symbol west-node :west)]
+                     {:north? north? :south? south? :east? east? :west? west?}))
         follow-path (fn [line line-index pos-index]
                       (let [next-line (nth lines (inc line-index) "...")
                             prev-line (nth lines (dec line-index) "...")
                             next-nodes (get-path line (:line next-line) (:line prev-line) pos-index)]
+                        
                         (print next-nodes)))
         _ (print :s-line s-line :s-index s-index)
-        _ (println follow-path)
+        _ (println (follow-path (:line s-line) (:line-index s-line) s-index))
         ]
     ))
 
